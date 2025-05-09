eb394fd244951d9a40085123f78556afe0cbeabb
MINDHELL <Tgpurpose640@gmail.com>
2025-05-09 14:43:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -260,7 +260,7 @@ async def send_random_video(client, chat_id):
 @bot.on_callback_query(filters.regex("get_random_video"))
 async def random_video_callback(client, callback_query: CallbackQuery):
     await callback_query.answer()
-    await send_random_video(client, callback_query.message.chat.id))
+    await send_random_video(client, callback_query.message.chat.id)
 
 
 # ✅ **Quota Status**
